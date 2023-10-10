5f348c1b422903fe1c8e2707093a961ceb9304cd
GOLDEN BUDDHA3W83W8⬆️ <sbibek086@gmail.com>
2023-10-10 14:07:43 +0545

    Update Open_NOT_in_default_incognito.py

--- Open_NOT_in_default_incognito.py
+++ Open_NOT_in_default_incognito.py
@@ -1,1 +1,10 @@
-sd
+import os
+from playwright.sync_api import Playwright, sync_playwright, expect, Page
+
+def run(page: Page) -> None:
+    app_data_path = os.getenv("LOCALAPPDATA")
+    user_data_path = os.path.join(app_data_path, 'Chromium\\User Data\\Default')
+    context= playwright.chromium.launch_persistent_context(user_data_path, channel = 'msedge',  headless= False)
+    page = context.new_page()
+    page.goto("https://ndrrma.yilab.org.np/")
+    page.wait_for_timeout(5000)

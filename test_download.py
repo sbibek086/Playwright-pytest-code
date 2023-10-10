88c5e68d3e3ca3ac218d678641525189c7ddded0
GOLDEN BUDDHA3W83W8⬆️ <sbibek086@gmail.com>
2023-10-10 16:22:36 +0545

    Update test_download.py

--- test_download.py
+++ test_download.py
@@ -1,19 +1,21 @@
+import os
+
 from playwright.sync_api import Playwright, sync_playwright, expect, Page
 
 def test_run(page: Page) -> None:
     page.goto("https://ndrrma.yilab.org.np/np/daily-bulletin/1145")
     with page.expect_download() as download_i:
-        page.locator("div").click()
+        page.locator("2023-08-11").click()
     dl = download_i.value
 
-print(dl.path())
+    print(dl.path())
 
-page.wait_for_timeout(5000)
+    page.wait_for_timeout(5000)
 
 #default download downloads in odd locations. to put it in favorable location,
 
-working_dir_path = os.getcwd()
+    working_dir_path = os.getcwd()
 
-final_path = os.path.join(working_dir_path, "<fileLocation>/<Filename.txt>")
+    final_path = os.path.join(working_dir_path, "./testdata/mydownloadedFileContent.txt")
 
-dl.save_as(final_path)
+    dl.save_as(final_path) 
